e7c8d9f0622aa7dc089f31c57b8c769a9dbdfb7d
fast050 <dragonman050@gmail.com>
2022-04-21 16:57:08 +0400

    - create custom view add attrs in your xml

--- app/src/main/java/com/example/customview/CustomView.kt
+++ app/src/main/java/com/example/customview/CustomView.kt
@@ -9,7 +9,7 @@ import android.util.AttributeSet
 import android.view.View
 
 class CustomView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet?,
+    context: Context, attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
@@ -17,37 +17,55 @@ class CustomView @JvmOverloads constructor(
 
     //to smooth the text drawing (look it up)
     private var paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    //add color to your shape in your xml
+    private var squareColor: Int = 0
+    //add size of your shape in your xml
+    private var squareSize: Int =0
 
     init {
-        paint.color = Color.GREEN
+        if (attrs != null) {
+
+            //to use your xml attrs in your xml
+            val arrayType = getContext().obtainStyledAttributes(attrs, R.styleable.CustomView)
+
+            squareColor = arrayType.getColor(R.styleable.CustomView_square_color, SHAPE_INIT_COLOR)
+            squareSize =
+                arrayType.getDimensionPixelSize(R.styleable.CustomView_square_size, SHAPE_SIZE)
+
+            paint.color = squareColor
+            //to clear the arrayType object
+            arrayType.recycle()
+        }
     }
 
     fun swapColor() {
-        paint.color = if (paint.color == Color.GREEN)
+        paint.color = if (paint.color == squareColor)
             Color.RED
         else
-            Color.GREEN
+            squareColor
 
-        //dont block the ui
+        //don't block the ui
         postInvalidate()
 
         //invalidate()
     }
 
+    //it call a lot pre second
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
         //rec.top is represent the space from X,Y axis
         rec.top = 0
         rec.left = 0
-        rec.bottom = rec.top + SHAPE_SIZE
-        rec.right = rec.left + SHAPE_SIZE
+        rec.bottom = rec.top + squareSize
+        rec.right = rec.left + squareSize
 
 
         canvas?.drawRect(rec, paint)
     }
 
     companion object {
+        const val SHAPE_INIT_COLOR = Color.GREEN
         const val SHAPE_SIZE = 100
     }
 }
